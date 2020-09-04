3b85f77e710802e33800681fbe103eba32d24533
Dermuir <34244111+dermuir@users.noreply.github.com>
2020-09-04 12:52:22 -0500

    Add files via upload

---
+++ Truffle/contracts/testsol.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+contract testsol{
+
+    string _name;
+
+    constructor (string name) public
+    {
+        _name = name;
+        
+    }
+
+}
---
+++ Truffle/migrations/1_initial_migration.js
@@ -0,0 +1,8 @@
+var Migrations = artifacts.require("./Migrations.sol");
+var testsol = artifacts.require("./testsol.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
+  deployer.deploy(testsol,"Luis");
+};
+
